8676736466b1955a9cd99dfb32c066c2eb93ae93
daandupau <dw.dupau@gmail.com>
2018-05-17 16:02:57 +0200

    Modified script to make it compatible with php

--- Scripts/initial-install/zfs-epel.sh
+++ Scripts/initial-install/zfs-epel.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
 ####################################################
 zfsrepo=/etc/yum.repos.d/zfs.repo
-OUTPUT="input.txt"
+zfs=${$1}
 
 ####################################################
-
-# dialog for epel release
->$OUTPUT
-trap "rm $OUTPUT; exit" SIGHUP SIGINT SIGTERM
-
-dialog --title "zfs-epel" \
---input-box "For latest release: https://github.com/zfsonlinux/zfs/wiki/RHEL-and-CentOS" 10 150 2>$OUTPUT
-respose=$?
-
-ZFSURL=$(<$OUTPUT)
-case $respose in
-  0) 
-  	zfs=${ZFSURL}
-  	;;
-  1) 
-  	echo "Cancel pressed." 
-  	;;
-  255) 
-   echo "[ESC] key pressed."
-esac
 
 yum install $zfs -y
 gpg --quiet --with-fingerprint /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
